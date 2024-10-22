e9dc7157e4c5ab6bed1351cbaab8b260b941ad02
Rameez <114074269+mRameez110@users.noreply.github.com>
2024-10-22 21:12:55 +0500

    use shortid instead of nanoid in controller/url.js

--- controllers/url.js
+++ controllers/url.js
@@ -1,9 +1,9 @@
-const { nanoid } = require("nanoid");
+const shortid = require("shortid");
 const URL = require("../models/url");
 async function handleGenerateNewShortURL(req, res) {
   const body = req.body;
   if (!body.url) return res.status(400).json({ error: "Url is required" });
-  const shortID = nanoid(8);
+  const shortID = shortid;
   await URL.create({
     shortId: shortID,
     redirectURL: ReportBody.url,
